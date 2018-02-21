62f5130203b86047af42333d2bb8b0a06f119352
CosimoLovascio <cosimolovascio@hotmail.it>
2018-02-21 14:34:57 +0100

    Added calculate field in configuration file

--- README.md
+++ README.md
@@ -7,3 +7,46 @@ Crowd Pulse lemmatizer implemented with Stanford CoreNLP.
 
 The `lemmatizer-stanford` plugin needs additional lemmatization models in the classpath (see 
 http://nlp.stanford.edu/software/corenlp.shtml).
+
+You can specify the configuration option "calculate" with one of the following values:
+- all: lemmatize all tokens coming from the stream;
+- new: lemmatize the tokens with no lemma (property is null);
+
+Example of usage:
+
+```json
+{
+  "process": {
+    "name": "lemmatizer-tester",
+    "logs": "/opt/crowd-pulse/logs"
+  },
+  "nodes": {
+    "message-fetcher": {
+      "plugin": "message-fetch",
+      "config": {
+        "db": "test"
+      }
+    },
+    "lemmatizer": {
+      "plugin": "lemmatizer-stanford",
+      "config": {
+        "calculate": "new"
+      }
+    },
+    "message-persister": {
+      "plugin": "message-persist",
+      "config": {
+        "db": "test"
+      }
+    }
+  },
+  "edges": {
+    "message-fetcher": [
+      "lemmatizer"
+    ],
+    "lemmatizer": [
+      "message-persister"
+    ]
+  }
+}
+```
--- src/main/java/com/github/frapontillo/pulse/crowd/lemmatize/corenlp/CoreNLPLemmatizer.java
+++ src/main/java/com/github/frapontillo/pulse/crowd/lemmatize/corenlp/CoreNLPLemmatizer.java
@@ -3,9 +3,13 @@ package com.github.frapontillo.pulse.crowd.lemmatize.corenlp;
 import com.github.frapontillo.pulse.crowd.data.entity.Message;
 import com.github.frapontillo.pulse.crowd.data.entity.Token;
 import com.github.frapontillo.pulse.crowd.lemmatize.ILemmatizerOperator;
+import com.github.frapontillo.pulse.spi.IPluginConfig;
 import com.github.frapontillo.pulse.spi.ISingleablePlugin;
-import com.github.frapontillo.pulse.spi.VoidConfig;
+import com.github.frapontillo.pulse.spi.PluginConfigHelper;
+import com.github.frapontillo.pulse.util.PulseLogger;
+import com.google.gson.JsonElement;
 import edu.stanford.nlp.process.Morphology;
+import org.apache.logging.log4j.Logger;
 import rx.Observable;
 
 import java.util.List;
@@ -19,23 +23,36 @@ import java.util.List;
  *
  * @author Francesco Pontillo
  */
-public class CoreNLPLemmatizer extends ISingleablePlugin<Message, VoidConfig> {
+public class CoreNLPLemmatizer extends ISingleablePlugin<Message, CoreNLPLemmatizer.CoreNLPLemmatizerConfig> {
     public final static String PLUGIN_NAME = "lemmatizer-stanford";
     private Morphology morphology;
+
+    private final static Logger logger = PulseLogger.getLogger(CoreNLPLemmatizer.class);
 
     @Override public String getName() {
         return PLUGIN_NAME;
     }
 
-    @Override public VoidConfig getNewParameter() {
-        return new VoidConfig();
+    @Override public CoreNLPLemmatizerConfig getNewParameter() {
+        return new CoreNLPLemmatizerConfig();
     }
 
-    @Override public Observable.Operator<Message, Message> getOperator(VoidConfig parameters) {
+    @Override public Observable.Operator<Message, Message> getOperator(CoreNLPLemmatizerConfig parameters) {
         CoreNLPLemmatizer currentLemmatizer = this;
         return new ILemmatizerOperator(this) {
             @Override public List<Token> lemmatizeMessageTokens(Message message) {
-                return currentLemmatizer.singleItemProcess(message).getTokens();
+                if (parameters != null) {
+                    switch (parameters.getCalculate()) {
+                        case CoreNLPLemmatizerConfig.ALL:
+                            return currentLemmatizer.singleItemProcess(message).getTokens();
+                        case CoreNLPLemmatizerConfig.NEW:
+                            return currentLemmatizer.singleItemProcessWithFilter(message).getTokens();
+                        default:
+                            return currentLemmatizer.singleItemProcess(message).getTokens();
+                    }
+                } else {
+                    return currentLemmatizer.singleItemProcess(message).getTokens();
+                }
             }
         };
     }
@@ -52,6 +69,20 @@ public class CoreNLPLemmatizer extends ISingleablePlugin<Message, VoidConfig> {
         return message;
     }
 
+    public Message singleItemProcessWithFilter(Message message) {
+        if (message.getTokens() != null) {
+            message.getTokens().forEach(token -> {
+                if (!token.isStopWord() && token.getLemma() == null) {
+                    String lemma = getMorphology().lemma(token.getText(), token.getPos());
+                    token.setLemma(lemma);
+                } else {
+                    logger.info("Token skipped (lemma already exist)");
+                }
+            });
+        }
+        return message;
+    }
+
     /**
      * Get the existing or build a CoreNLP {@link Morphology} if none exists.
      *
@@ -62,5 +93,37 @@ public class CoreNLPLemmatizer extends ISingleablePlugin<Message, VoidConfig> {
             morphology = new Morphology();
         }
         return morphology;
+    }
+
+    class CoreNLPLemmatizerConfig implements IPluginConfig<CoreNLPLemmatizerConfig> {
+
+        /**
+         * Lemmatize of all tokens coming from the stream.
+         */
+        public static final String ALL = "all";
+
+        /**
+         * Lemmatize the tokens with no lemma (property is null).
+         */
+        public static final String NEW = "new";
+
+
+        /**
+         * Accepted values: NEW, ALL
+         */
+        private String calculate;
+
+        @Override
+        public CoreNLPLemmatizerConfig buildFromJsonElement(JsonElement jsonElement) {
+            return PluginConfigHelper.buildFromJson(jsonElement, CoreNLPLemmatizerConfig.class);
+        }
+
+        public String getCalculate() {
+            return calculate;
+        }
+
+        public void setCalculate(String calculate) {
+            this.calculate = calculate;
+        }
     }
 }
